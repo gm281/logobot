c466c30b56f7a5d53274cc5a7b418e8f80251373
Grzegorz Miłoś <grzegorz.milos@bromium.com>
2013-09-07 23:10:36 +0100

    Add simple server (arduino) / client (external machine). The client reads keystrokes and forwards to the server, which then writes them to the serial line (for Arduino's reception). This works with test.ino test sketch.

---
+++ RaspberryPi/client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import time
+import socket               # Import socket module
+import sys
+import os, fcntl, tty, termios
+
+def getch():
+    fd = sys.stdin.fileno()
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    try:
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        return sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSANOW, oldterm)
+
+s = socket.socket()         # Create a socket object
+host = '192.168.0.201' #socket.gethostname() # Get local machine name
+port = 12348                # Reserve a port for your service.
+
+print 'Connecting to ', host, port
+s.connect((host, port))
+
+print 'Waiting for input'
+while True:
+    byte = getch()
+    print 'Got input "%c" sending off' % byte
+    s.send(byte)
---
+++ RaspberryPi/server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python           
+ 
+import socket               # Import socket module
+s = socket.socket()         # Create a socket object
+host = '192.168.0.201'	    # Get local machine name
+port = 12348                # Reserve a port for your service.
+ 
+import serial
+import time
+ 
+DEVICE = '/dev/ttyACM0' # the arduino serial interface (use dmesg when connecting)
+BAUD = 9600
+ser = serial.Serial(DEVICE, BAUD)
+ 
+ 
+s.bind((host, port))        # Bind to the port
+print 'Server started!'
+while True:
+    print 'Waiting for client'
+    s.listen(5)                 # Now wait for client connection.
+    c, addr = s.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    while True:
+        msg = c.recv(1) # get a byte from the TCP connection
+        if msg == "":
+            print 'Connection closed'
+            c.close() 
+            break
+        print 'Got "%s", writing to serial' % msg
+        ser.write(msg)  # write the byte to the serial interface
